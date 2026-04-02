39bf5cd33bbd577b63ca2ed3f6b40fc4a6b7b3b8
GISADan-eng <d.gisa2@alustudent.com>
2026-04-02 12:26:23 +0000

    Add task 1 - export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -15,20 +15,16 @@ if __name__ == "__main__":
 
     employee_id = argv[1]
 
-    # Fetch employee info
     user = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     ).json()
 
-    # Fetch all TODOs for the employee
     todos = requests.get(
         f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     ).json()
 
-    # CSV filename: <employee_id>.csv
     filename = f"{employee_id}.csv"
 
-    # Write all tasks to CSV
     with open(filename, mode="w", newline="") as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in todos:
